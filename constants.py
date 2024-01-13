9cc6eb5f6d97ed2331af3ce2782722fa94effeb9
terru3 <terru3@hotmail.com>
2024-01-12 18:01:38 -0800

    prepare utils and workflow for pruning

--- constants.py
+++ constants.py
@@ -1,10 +1,9 @@
 SEED = 42
-EPOCHS = 100
+EPOCHS = 100 ## 100 during original training
 BATCH_SIZE = 512
 LR = 1e-3
 
-#### TODO: REMINDER to self that for now we are training a regular model till convergence
-PRUNE_PCT = 0
+PRUNE_PCT = 80
 
 PRUNE_ITERS = 5
 # # if PRUNE_PCT = 10, PRUNE_ITERS = 5, we want 90% of model params left at the end, so each iter we keep 0.9^(1/5) ≈ 97.9% of model
